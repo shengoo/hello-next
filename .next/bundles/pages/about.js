baecb1e2d3abe1607a2d69e1535ef5f3ba2a2a52
shengqing <shengqing@babytree-inc.com>
2018-01-09 17:15:58 +0800

    delete files

--- .next/bundles/pages/about.js
+++
@@ -1,6 +0,0 @@
-
-          window.__NEXT_REGISTER_PAGE('/about', function() {
-            var comp = module.exports=webpackJsonp([9],{209:function(e,t,u){e.exports=u(210)},210:function(e,t,u){"use strict";function n(e){return e&&e.__esModule?e:{default:e}}Object.defineProperty(t,"__esModule",{value:!0});var l=u(1),a=n(l),o=u(37),r=n(o);t.default=function(){return a.default.createElement(r.default,null,a.default.createElement("p",null,"This is about page"))}}},[209]);
-            return { page: comp.default }
-          })
-        
